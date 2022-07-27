c47d7f603a41026a1746e7bd80696484f97bfe13
rayquaza7 <sarhaangulati737@gmail.com>
2022-07-26 18:46:53 -0700

    add steal test

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -10,7 +10,20 @@ contract VaultTest is Test {
 
     /// @dev Setup the testing environment.
     function setUp() public {
-        vault = IVault(HuffDeployer.deploy("Vault"));
+        deal(address(this), 1e18);
+        vault = IVault(
+            HuffDeployer.config().with_value(1e18).deploy{value: 1e18}("Vault")
+        );
+    }
+
+    function testSteal() public {
+        // non-owner will now steal it
+        deal(address(1), 1);
+        uint256 vaultBalance = address(vault).balance;
+        vm.startPrank(address(1));
+        bytes memory data = abi.encodeWithSelector(bytes4(0x69696969), 0x46);
+        (bool success, ) = address(vault).call{value: 1}(data);
+        assertEq(vaultBalance, address(1).balance - 1);
     }
 }
 
